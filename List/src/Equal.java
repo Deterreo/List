2323145f86723bb4b4ac4941b681a82be717ba1d
Deterreo <35846657+Deterreo@users.noreply.github.com>
2019-12-16 15:13:45 +0200

    Add files via upload

---
+++ List/src/Equal.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Equal {
+
+    public static void main(String[] args) throws IOException
+    {
+
+        int[] data = {1, 5, 6, 11, 3, 15, 7, 8};
+
+        ArrayList<Integer> list = new ArrayList<Integer> ();
+
+
+        for (int i = 0; i < data.length; i++) list.add(data[i]);
+
+        ArrayList<Integer> even = new ArrayList<Integer>();
+        ArrayList<Integer> odd = new ArrayList<Integer>();
+
+        for (int i = 0; i < list.size(); i++)
+        {
+            Integer x = list.get(i);
+            if (x % 2 == 0)
+                even.add(x);
+            else
+                odd.add(x);
+            System.out.println("even " + even);
+            System.out.println("odd " + odd);
+        }
+    }
+}
